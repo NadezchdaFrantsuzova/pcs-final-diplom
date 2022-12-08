6bf248f960c4b769e00233dc5924ff1d6b8ab2a5
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-12-08 15:44:26 +0300

    в класс Main написан сервер, добавлен класс Client, внесены изменения в класс BooleanSearchEngine, добавлена зависимость в файл pom.xml

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,64 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    Map<String, List<PageEntry>> result = new HashMap<>();
 
-    public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+    public BooleanSearchEngine(File pdfsDir) {
+
+        for (File pdf : pdfsDir.listFiles()) {
+
+            String pdfName = pdf.getName();
+
+            try (PdfDocument doc = new PdfDocument(new PdfReader(pdf))) {
+
+                int page = doc.getNumberOfPages();
+                Map<String, Integer> freqs = new HashMap<>();
+                List<PageEntry> pageEntryList;
+
+                for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+
+                    var text = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+                    var words = text.split("\\P{IsAlphabetic}+");
+
+                    for (var word : words) {
+                        if (word.isEmpty()) {
+                            continue;
+                        }
+                        word = word.toLowerCase();
+                        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                    }
+                }
+                for (var kv : freqs.entrySet()) {
+                    String wordResult = kv.getKey();
+                    int count = kv.getValue();
+
+                    if (result.containsKey(wordResult)) {
+                        pageEntryList = result.get(wordResult);
+                    } else {
+                        pageEntryList = new ArrayList<>();
+                    }
+                    pageEntryList.add(new PageEntry(pdfName, page, count));
+
+                    Collections.sort(pageEntryList, Collections.reverseOrder());
+                    result.put(wordResult, pageEntryList);
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        return result.get(word);
     }
 }
